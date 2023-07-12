705a49a2f6eeb2117b29c50f312cffd554f7eb67
roblkdeboer <roblkdeboer@gmail.com>
2023-07-12 18:50:32 +0800

    Add env variable for prod/dev

--- main.go
+++ main.go
@@ -3,19 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func getData(w http.ResponseWriter, req *http.Request) {
   fmt.Fprintf(w, "Hello world\n")
 }
 
-func main() {	
-	http.HandleFunc("/data", getData)
+func main() {
+	http.HandleFunc("/data", getData);
 
-	http.ListenAndServeTLS(
-		":443",
-		"/etc/letsencrypt/live/bankapi.roblkdeboer.com/fullchain.pem",
-		"/etc/letsencrypt/live/bankapi.roblkdeboer.com/privkey.pem",
-		nil,
-	  )
+	serverEnv := os.Getenv("SERVER_ENV")
+
+	if serverEnv == "DEV" {
+		http.ListenAndServe(":8080", nil)
+	} else if serverEnv == "PROD" {
+		http.ListenAndServeTLS(
+			":443",
+			"/etc/letsencrypt/live/bankapi.roblkdeboer.com/fullchain.pem",
+			"/etc/letsencrypt/live/bankapi.roblkdeboer.com/privkey.pem",
+			nil,
+		)
+	}
 }
