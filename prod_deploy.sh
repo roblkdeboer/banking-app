f6e45646a3133c1099dd1ba5b82f96f435650dd4
roblkdeboer <roblkdeboer@gmail.com>
2023-07-18 17:32:53 +0800

    Update prod deploy script

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -13,9 +13,9 @@ git pull
 msg "Building Docker image"
 sudo docker build --tag banking-app .
 
-# msg "Stopping Docker container"
-# sudo docker stop banking-app
-# sudo docker rm banking-app
+msg "Stopping Docker container"
+sudo docker stop banking-app
+sudo docker rm banking-app
 
 msg "Starting Docker container"
 sudo docker run \
