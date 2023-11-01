15976ef9d1c399fb61be3c320f9af79512903fec
roblkdeboer <roblkdeboer@gmail.com>
2023-11-01 22:01:33 +0800

    Create user accounts function placeholder

---
+++ handlers/accounts.go
@@ -0,0 +1,13 @@
+package handlers
+
+import "net/http"
+
+// TO DO: Create Savings Account
+func CreateSavingsAccount(w http.ResponseWriter, req *http.Request) {
+
+}
+
+// TO DO: Create Current Account
+func CreateCurrentAccount(w http.ResponseWriter, req *http.Request) {
+
+}
